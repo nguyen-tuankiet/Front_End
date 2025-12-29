7feee2bcf0eded889fd4d462dcce361183815bd5
kiet <22130136@st.hcmuaf.edu.vn>
2025-12-29 16:25:14 +0700

    pull

--- src/services/api.js
+++ src/services/api.js
@@ -4,8 +4,8 @@
  */
 
 // Base URL - Change this when switching environments
-// const BASE_URL = "https://backend-laptrinhfrontend.onrender.com";
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://backend-laptrinhfrontend.onrender.com";
+// const BASE_URL = "http://localhost:8080";
 
 // Default language
 const DEFAULT_LANG = "vi";
