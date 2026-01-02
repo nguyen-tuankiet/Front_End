fb81f161cd22aa48df65ffdcb5e2a87590984b93
TuanKhoaNLU <22130123@st.hcmuaf.edu.vn>
2026-01-02 09:50:55 +0700

    Trang lưu bài báo + chức năng lưu

--- src/components/ui/SaveButton.jsx
+++ src/components/ui/SaveButton.jsx
@@ -1,24 +1,61 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { cn } from "@/lib/utils";
 import { Bookmark, BookmarkCheck } from "lucide-react";
 import { Button } from "./button";
+import { saveArticle, removeSavedArticle, isArticleSaved } from "@/lib/savedArticles";
 
 /**
  * Component nút lưu bài viết
  * @param {Object} props
  * @param {string|number} props.articleId - ID bài viết
+ * @param {Object} props.article - Dữ liệu bài viết
  * @param {string} props.className - CSS class bổ sung
- * @param {Function} props.onSave - Callback khi lưu
+ * @param {Function} props.onSave - Callback khi lưu/xóa lưu
  */
-export function SaveButton({ articleId, className, onSave }) {
+export function SaveButton({ articleId, article, className, onSave }) {
+    const articleUrl = article?.url || article?.link || articleId;
     const [isSaved, setIsSaved] = useState(false);
 
-    const handleSave = () => {
-        setIsSaved(!isSaved);
-        if (onSave) {
-            onSave(articleId, !isSaved);
+    // Kiểm tra trạng thái lưu
+    const checkSavedStatus = () => {
+        if (articleUrl) {
+            setIsSaved(isArticleSaved(articleUrl));
         }
-        // Xử lý lưu
+    };
+
+    useEffect(() => {
+        // eslint-disable-next-line react-hooks/set-state-in-effect
+        checkSavedStatus();
+
+        // Nghe event khi có thay đổi từ component khác
+        const handleSavedArticlesChange = () => {
+            checkSavedStatus();
+        };
+
+        window.addEventListener('savedArticlesChanged', handleSavedArticlesChange);
+        window.addEventListener('storage', handleSavedArticlesChange); // Cho trường hợp multi-tab
+
+        return () => {
+            window.removeEventListener('savedArticlesChanged', handleSavedArticlesChange);
+            window.removeEventListener('storage', handleSavedArticlesChange);
+        };
+    }, [articleUrl]);
+
+    const handleSave = () => {
+        const newSavedState = !isSaved;
+        setIsSaved(newSavedState);
+
+        if (articleUrl) {
+            if (newSavedState && article) {
+                saveArticle(article);
+            } else {
+                removeSavedArticle(articleUrl);
+            }
+        }
+
+        if (onSave) {
+            onSave(articleId || articleUrl, newSavedState);
+        }
     };
 
     return (
